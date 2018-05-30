eb0c940972f1238516aaf982117664f6f708171d
pchriste24 <pchriste24@gmail.com>
2018-05-29 23:30:02 -0500

    Initial Commit

---
+++ TaxiMachineLearning/Program.cs
@@ -0,0 +1,60 @@
+﻿using System;
+using Microsoft.ML.Models;
+using Microsoft.ML.Trainers;
+using Microsoft.ML.Transforms;
+using Microsoft.ML;
+using System.Threading.Tasks;
+
+namespace TaxiMachineLearning
+{
+    class Program
+    {
+        const string _datapath = @".\Data\taxi-fare-train.csv";
+        const string _testdatapath = @".\Data\taxi-fare-test.csv";
+        const string _modelpath = @".\Data\Model.zip";
+
+       static void Main(string[] args)
+        {
+            PredictionModel<TaxiTrip, TaxiTripFarePrediction> model = Train().Result;
+            Evaluate(model);
+
+            var prediction = model.Predict(TestTrips.Trip1);
+            Console.WriteLine("Predicted fare: {0}, actual fare: 29.5", prediction.fare_amount);
+        }
+
+        public static async Task<PredictionModel<TaxiTrip, TaxiTripFarePrediction>> Train()
+        {
+            var pipeline = new LearningPipeline();
+            pipeline.Add(new TextLoader<TaxiTrip>(_datapath, useHeader: true, separator: ","));
+            pipeline.Add(new ColumnCopier(("fare_amount", "Label")));
+            pipeline.Add(new CategoricalOneHotVectorizer("vendor_id",
+                                             "rate_code",
+                                             "payment_type"));
+            pipeline.Add(new ColumnConcatenator("Features",
+                                    "vendor_id",
+                                    "rate_code",
+                                    "passenger_count",
+                                    "trip_distance",
+                                    "payment_type"));
+
+            pipeline.Add(new FastTreeRegressor());
+
+            PredictionModel<TaxiTrip, TaxiTripFarePrediction> model = pipeline.Train<TaxiTrip, TaxiTripFarePrediction>();
+            await model.WriteAsync(_modelpath);
+            return model;
+        }
+        private static void Evaluate(PredictionModel<TaxiTrip, TaxiTripFarePrediction> model)
+        {
+            var testData = new TextLoader<TaxiTrip>(_testdatapath, useHeader: true, separator: ",");
+
+            var evaluator = new RegressionEvaluator();
+            RegressionMetrics metrics = evaluator.Evaluate(model, testData);
+            // Rms should be around 2.795276
+            Console.WriteLine("Rms=" + metrics.Rms);
+
+            Console.WriteLine("RSquared = " + metrics.RSquared);
+
+        }
+
+    }
+}
---
+++ TaxiMachineLearning/TaxiTrip.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using Microsoft.ML.Runtime.Api;
+namespace TaxiMachineLearning
+{
+    public class TaxiTrip
+    {
+        [Column(ordinal: "0")]
+        public string vendor_id;
+        [Column(ordinal: "1")]
+        public string rate_code;
+        [Column(ordinal: "2")]
+        public float passenger_count;
+        [Column(ordinal: "3")]
+        public float trip_time_in_secs;
+        [Column(ordinal: "4")]
+        public float trip_distance;
+        [Column(ordinal: "5")]
+        public string payment_type;
+        [Column(ordinal: "6")]
+        public float fare_amount;
+    }
+
+    public class TaxiTripFarePrediction
+    {
+        [ColumnName("Score")]
+        public float fare_amount;
+    }
+}
